f0f70ed830a68e159231fb60e27d549e7f74cf7d
Сорока Дмитрий Николаевич (4038347) <soroka@dev.vtb.ru>
2021-03-04 10:12:26 +0400

    Обновление ValidatorFactory

--- app/src/main/java/com/rental/transport/service/PropertyService.java
+++ app/src/main/java/com/rental/transport/service/PropertyService.java
@@ -1,21 +1,16 @@
 package com.rental.transport.service;
 
-import android.view.View;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.rental.transport.R;
 import com.rental.transport.adapter.PropertyListAdapter;
 import com.rental.transport.model.Property;
 import com.rental.transport.validator.IStringValidator;
 import com.rental.transport.validator.ValidatorFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class PropertyService {
-
+    ValidatorFactory vf = new ValidatorFactory();
     private static PropertyService mInstance;
 
     public PropertyService() {
@@ -55,8 +50,7 @@ public class PropertyService {
     public IStringValidator getValidator(List<Property> props, String name) throws IllegalArgumentException {
 
         Property prop = searchProperty(props, name);
-        ValidatorFactory factory = new ValidatorFactory();
-        return factory.getValidator(prop.getType());
+        return vf.getValidator(prop.getType());
     }
 
     public List<Property> getPropertyFromList(ListView listView) {

efe76d12782542af6e28b0222dda84428eb2782c
soroka.dmitry <krafft1981@mail.ru>
2021-05-11 20:38:02 +0400

    sdf

--- app/src/main/java/com/rental/transport/activity/TransportDetails.java
+++ app/src/main/java/com/rental/transport/activity/TransportDetails.java
@@ -52,21 +52,4 @@ public class TransportDetails extends Fragment {
 
         return root;
     }
-
 }
-
-//public class TransportDetails extends Fragment implements AdapterView.OnItemSelectedListener {
-//
-//    @Override
-//    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-//        // TODO Auto-generated method stub
-//    }
-//
-//    @Override
-//    public void onNothingSelected(AdapterView<?> arg0) {
-//        // TODO Auto-generated method stub
-//    }
-//}
-//    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-//    photoPickerIntent.setType("image/*");
-//    startActivityForResult(photoPickerIntent, SELECT_PHOTO);

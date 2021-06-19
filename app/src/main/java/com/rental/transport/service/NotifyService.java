fd26c2dbe074568aa535865f042ad1d342947a41
soroka.dmitry <krafft1981@mail.ru>
2021-06-19 17:59:50 +0400

    bug fix

--- app/src/main/java/com/rental/transport/service/NotifyService.java
+++ app/src/main/java/com/rental/transport/service/NotifyService.java
@@ -1,14 +1,77 @@
 package com.rental.transport.service;
 
+import com.rental.transport.model.Customer;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import tech.gusavila92.websocketclient.WebSocketClient;
+
 public class NotifyService {
 
+    private WebSocketClient webSocketClient = Connect();
     private static NotifyService mInstance;
 
     public NotifyService() {
     }
 
-    private void Connect() {
+    private WebSocketClient Connect() {
 
+        System.out.println("Connect to WebSocket");
+
+        URI uri;
+        try {
+            uri = new URI("ws://138.124.187.10:8080/websocket");
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        WebSocketClient client = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                Customer customer = MemoryService.getInstance().getCustomer();
+                send(customer.getId().toString() + " " + customer.getAccount());
+            }
+
+            @Override
+            public void onTextReceived(String message) {
+                System.out.println("onTextReceived");
+            }
+
+            @Override
+            public void onBinaryReceived(byte[] data) {
+                System.out.println("onBinaryReceived");
+            }
+
+            @Override
+            public void onPingReceived(byte[] data) {
+                System.out.println("onPingReceived");
+            }
+
+            @Override
+            public void onPongReceived(byte[] data) {
+                System.out.println("onPongReceived");
+            }
+
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onCloseReceived() {
+                System.out.println("onCloseReceived");
+            }
+        };
+
+        client.setConnectTimeout(5000);
+        client.setReadTimeout(60000);
+        client.enableAutomaticReconnection(5000);
+        client.connect();
+
+        return client;
     }
 
     public static NotifyService getInstance() {

13c1e23afafc2f49a3194fa28e0de2b05f006b65
Сорока Дмитрий Николаевич (4038347) <soroka@dev.vtb.ru>
2021-07-05 09:48:20 +0400

    bug fix

--- app/src/main/java/com/rental/transport/activity/MapFragment.java
+++ app/src/main/java/com/rental/transport/activity/MapFragment.java
@@ -1,23 +1,16 @@
 package com.rental.transport.activity;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -43,27 +36,21 @@ public class MapFragment extends Fragment {
             e.printStackTrace();
         }
 
-        view.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
+        view.getMapAsync(mMap -> {
+            googleMap = mMap;
 
-                String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+//            googleMap.setMyLocationEnabled(true);
 
-//                googleMap.setMyLocationEnabled(true);
+            LatLng place = new LatLng(0, 0);
+            googleMap.addMarker(new MarkerOptions()
+                    .position(place)
+                    .title("Стоянка")
+                    .snippet("Место сбора")
+            );
 
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions()
-                        .position(sydney)
-                        .title("Стоянка")
-                        .snippet("Место сбора")
-                );
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
+            // For zooming automatically to the location of the marker
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(place).zoom(12).build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         });
 
         return rootView;
